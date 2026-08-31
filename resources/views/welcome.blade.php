8458c23dd47d243c4a703e71ea848cd1b80782eb
Psychovv <viniciusmedina63@gmail.com>
2026-08-31 20:05:33 -0300

    feat: view Blade inicial (HTML puro)

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Artel</title>
+</head>
+<body>
+    <h1>Artel</h1>
+    <p>Aplicação de divisão de despesas em grupo.</p>
+</body>
+</html>
